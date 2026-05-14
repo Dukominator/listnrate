b78f6a23bf0cd41df43f30eb0c1ee32623003166
Dukominator <nielspp@gmail.com>
2026-05-14 20:53:57 +0200

    Updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
 import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <Navbar />
 
-      <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold mb-4">
-          Welcome to ListnRate Beta
+      <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center gap-6">
+        <h1 className="text-6xl font-bold">
+          Welcome to ListnRate
         </h1>
 
         <p className="text-zinc-400 text-xl">
           Create. Rate. Share.
         </p>
+
+        <Button>
+          Create Your First List
+        </Button>
       </main>
     </>
   );
