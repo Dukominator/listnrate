269fd1237bd0aec5d280038ceb303e46c25a7297
Dukominator <nielspp@gmail.com>
2026-05-25 23:54:06 +0200

    tmdb debug2

--- lib/tmdb.ts
+++ lib/tmdb.ts
@@ -1,16 +1,33 @@
+const API_KEY = process.env.TMDB_API_KEY;
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+async function fetchFromTMDB(endpoint: string) {
+  const res = await fetch(
+    `${BASE_URL}${endpoint}${
+      endpoint.includes("?") ? "&" : "?"
+    }api_key=${API_KEY}`
+  );
+
+  return await res.json();
+}
+
 export async function getTrendingMovies() {
-  console.log("🔥 TRENDING FUNCTION CALLED");
-
-  const url =
-    `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.TMDB_API_KEY}`;
-
-  console.log("🔥 URL:", url);
-
-  const response = await fetch(url);
-
-  const data = await response.json();
-
-  console.log("🔥 TMDB RESPONSE:", data);
+  const data = await fetchFromTMDB(
+    "/trending/movie/week"
+  );
 
   return data?.results ?? [];
+}
+
+export async function searchMovies(query: string) {
+  const data = await fetchFromTMDB(
+    `/search/movie?query=${encodeURIComponent(query)}`
+  );
+
+  return data?.results ?? [];
+}
+
+export async function getMovie(id: string) {
+  return await fetchFromTMDB(`/movie/${id}`);
 }
